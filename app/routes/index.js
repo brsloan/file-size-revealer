60c8fb666032085e2eefd7abc78131616b717844
Benjamin Sloan <brsloan@gmail.com>
2016-01-17 10:47:01 +0000

    Up and working.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,56 +2,26 @@
 
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var fs = require('fs');
 
-module.exports = function (app, passport) {
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+var type = upload.single('file');
 
-	var clickHandler = new ClickHandler();
+module.exports = function (app) {
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+		
+	app.route('/api/fileanalyze/')
+		.post(type, function(req, res){
+			var fileSize = req.file.size;
+			fs.unlink(req.file.path);
+			res.json({
+				fileSize: fileSize
+			});
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
